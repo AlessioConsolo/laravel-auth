da943aa27ee0d04a1116785374be6263b3ed1bdb
AlessioConsolo <alessioconsolo3@gmail.com>
2024-10-08 22:40:16 +0200

    [UPD] updated store and update crud

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Project;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProjectController extends Controller
 {
@@ -26,11 +27,20 @@ class ProjectController extends Controller
         $validated = $request->validate([
             'title' => 'required|string|max:255',
             'description' => 'required|string',
-            'image' => 'nullable|image'
+            'image' => 'nullable|image|mimes:jpg,jpeg,png|max:2048'
         ]);
 
+        if ($request->hasFile('image')) {
+
+            $imagePath = $request->file('image')->store('cover_images', 'public');
+            
+            $validated['cover_image'] = $imagePath;
+        }
+
+        // Crea il progetto con i dati validati
         $project = Project::create($validated);
 
+        // Reindirizza con un messaggio di successo
         return redirect()->route('admin.projects.index')->with('success', 'Progetto creato con successo');
     }
 
@@ -52,8 +62,19 @@ class ProjectController extends Controller
         $validated = $request->validate([
             'title' => 'required|string|max:255',
             'description' => 'required|string',
-            'image' => 'nullable|image'
+            'image' => 'nullable|image|mimes:jpg,jpeg,png|max:2048'
         ]);
+
+        if ($request->hasFile('image')) {
+
+            if ($project->cover_image) {
+                Storage::disk('public')->delete($project->cover_image);
+            }
+
+            $imagePath = $request->file('image')->store('cover_images', 'public');
+            
+            $validated['cover_image'] = $imagePath;
+        }
 
         $project->update($validated);
 
